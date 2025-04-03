6399c97d7f5f74c7eb58875dd4021a90b8861aea
MartinMNKG <martin.kotlarczik@gmail.com>
2025-04-03 16:50:41 +0200

    [+] add processing

--- src/main_launch_database.py
+++ src/main_launch_database.py
@@ -3,30 +3,45 @@ main_path = os.getcwd()
 
 
 launch = False 
+Processing = True 
+Time_shift= False  
+log = False 
+scaler= False
+
 
 fuel1 = "NH3"
 fuel2 = "H2"
 oxidizer = "O2:0.21, N2:0.79, AR : 0.01"
 
 Detailed_file = "./data/detailed.yaml"
+name_d = "Detailed"
 Reduced_file = "./data/reduced.yaml"
-
+name_r = "Reduced"
 gas_det = ct.Solution(Detailed_file)
 gas_red = ct.Solution(Reduced_file)
 _gas_det_copy = ct.Solution(Detailed_file)
 _gas_red_copy = ct.Solution(Reduced_file)
 
-## 0D reactor : 
+################
+## 0D reactor ##
+################
+Name_Folder = "0D"
+Path = Create_0D_directory(main_path,Name_Folder)
+pressure_0D = np.linspace(1,1,1).tolist()
+temperature_0D = np.linspace(1000,2000,5).tolist()
+phi_0D = np.round(np.linspace(0.8, 1.2, 5), 1).tolist()
+mixture_0D =np.linspace(0.85,0.85,1).tolist()
+tmax = 0.05
+dt= 1e-6
+case_0D = generate_test_cases_bifuel(pressure_0D,temperature_0D,phi_0D,mixture_0D)
 if launch == True : 
-    
-    Name_Folder = "0D"
-    Path = Create_0D_directory(main_path,Name_Folder)
-    pressure_0D = np.linspace(1,1,1).tolist()
-    temperature_0D = np.linspace(1000,2000,5).tolist()
-    phi_0D = np.round(np.linspace(0.8, 1.2, 5), 1).tolist()
-    mixture_0D =np.linspace(0.85,0.85,1).tolist()
-    tmax = 0.05
-    dt= 1e-6
-    case_0D = generate_test_cases_bifuel(pressure_0D,temperature_0D,phi_0D,mixture_0D)
-    Sim0D(gas_det,_gas_det_copy,fuel1,fuel2,oxidizer,case_0D,dt,tmax,"Detailed",Path)
-    Sim0D(gas_red,_gas_red_copy,fuel1,fuel2,oxidizer,case_0D,dt,tmax,"Reduced",Path)
+    #Launch 0D reactor Base
+    Sim0D(gas_det,_gas_det_copy,fuel1,fuel2,oxidizer,case_0D,dt,tmax,name_d,Path)
+    Sim0D(gas_red,_gas_red_copy,fuel1,fuel2,oxidizer,case_0D,dt,tmax,name_r,Path)
+
+if Processing == True : 
+    #Processing Database
+    lenght = 500
+    csv_d = glob.glob(os.path.join(Path,f"{name_d}/*.csv"))
+    csv_r = glob.glob(os.path.join(Path,f"{name_r}/*.csv"))
+    Processing_0D(csv_d,csv_r,case_0D,Time_shift,log,scaler,lenght,name_d,name_r,Path) 
--- src/tools.py
+++ src/tools.py
@@ -1,15 +1,27 @@
 import numpy as np
 import cantera as ct
+import pandas as pd 
 import os
 import sys
 import time
-import warnings
 import itertools
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
-import pickle
+import glob
 
+class MinMaxScaler:
+    def fit(self, x):
+        self.min = x.min(0)
+        self.max = x.max(0)
 
+    def transform(self, x):
+        x = (x - self.min) / (self.max - self.min + 1e-7)
+        return x
+
+    def inverse_transform(self, x):
+        x = self.min + x * (self.max - self.min + 1e-7)
+        return x
+    
 def generate_test_cases_bifuel(pressure_range,temp_range, second_param,mixture):
     test_cases = list(itertools.product(pressure_range, temp_range, second_param,mixture))
 
@@ -61,3 +73,92 @@ def Sim0D(t_gas,gas_eq,fuel1,fuel2,oxidizer,case_0D,dt,tmax,type,dossier):
         )
 
     
+def Processing_0D(list_csv_d,list_csv_r,cases,time_shift,log,scaler,lenght,name_d,name_r,Path) : 
+
+    all_data_d = pd.DataFrame()
+    all_data_r = pd.DataFrame()
+    
+    list_csv = zip(list_csv_d,list_csv_r)
+    
+    for csv_d,csv_r in list_csv : 
+        
+        pressure, temperature, equivalence_ratio,mixture = cases[list_csv_d.index(csv_d)]
+        New_data_d = pd.DataFrame()
+        data_d=pd.read_csv(csv_d)
+        species_d = [col for col in data_d.columns if col.startswith("Y_")]
+        IDT_d = Calc_ai_delay(data_d["t"],data_d["T"])
+        if time_shift == True : 
+            data_d["t"] = data_d["t"] - IDT_d
+        
+        if log == True : 
+            data_d[species_d]=data_d[species_d].apply(np.log)  
+        
+        New_data_d["commun_grid"] = Generate_commun_grid(data_d["t"],lenght)
+        
+        for s in species_d : 
+            int_func = interp1d(data_d["t"],data_d[s],fill_value='extrapolate')
+            New_data_d[s] = int_func(New_data_d["commun_grid"])
+        
+        all_scl =[]
+        if scaler== True : 
+            for s in species_d : 
+                scl = MinMaxScaler()
+                scl.fit(New_data_d[s])
+                New_data_d[s] = scl.transform(New_data_d[s])
+                all_scl.append(scl)
+    
+        
+        New_data_d["T"]= data_d["T"]
+        New_data_d["IDT"] = IDT_d
+        New_data_d["P_Init"]  = pressure
+        New_data_d["T_Init"]  = temperature
+        New_data_d["phi_ini"]  =   equivalence_ratio  
+        New_data_d["mixt_init"] = mixture           
+        
+        all_data_d = pd.concat([all_data_d,New_data_d],ignore_index=True)
+    
+        
+        New_data_r = pd.DataFrame()
+        data_r=pd.read_csv(csv_r)
+        species_r = [col for col in data_r.columns if col.startswith("Y_")]
+        IDT_r = Calc_ai_delay(data_r["t"],data_r["T"])
+        if time_shift == True : 
+            data_r["t"] = data_r["t"] - IDT_r
+        if log == True : 
+            data_r[species_r]=data_r[species_r].apply(np.log)  
+        
+        New_data_r["commun_grid"] = Generate_commun_grid(data_r["t"],lenght)
+        
+        for s in species_r : 
+            int_func = interp1d(data_r["t"],data_r[s],fill_value='extrapolate')
+            New_data_r[s] = int_func(New_data_d["commun_grid"])
+            
+        if scaler== True : 
+            for s in species_r : 
+                scl = all_scl[species_d.index(s)]
+                New_data_r[s] = scl.transform(New_data_r[s])
+        
+        
+        New_data_r["T"]= data_r["T"]
+        New_data_r["IDT"] = IDT_r
+        New_data_r["P_Init"]  = pressure
+        New_data_r["T_Init"]  = temperature
+        New_data_r["phi_ini"]  =   equivalence_ratio  
+        New_data_r["mixt_init"] = mixture  
+    
+        all_data_r = pd.concat([all_data_r,New_data_r],ignore_index=True)     
+    
+    all_data_d.to_csv(os.path.join(Path,f"Processing_{name_d}.csv"))
+    all_data_r.to_csv(os.path.join(Path,f"Processing_{name_r}.csv"))
+            
+    
+def Calc_ai_delay(time,temp) :
+    loc_time = time
+    loc_temp = temp
+    diff_temp = np.diff(loc_temp)/np.diff(loc_time)
+    ign_pos = np.argmax(diff_temp)
+    output=loc_time[ign_pos]
+    return output
+
+def Generate_commun_grid(time,lenght) :
+    return np.linspace(min(time),max(time),lenght)
