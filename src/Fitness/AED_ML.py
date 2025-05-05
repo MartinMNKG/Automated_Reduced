2df162ac5b32df773d7d9b5290672d9a234bfd57
MartinMNKG <martin.kotlarczik@gmail.com>
2025-05-05 16:05:37 +0200

    [+] add Temperature to log

--- src/Fitness/AED_ML.py
+++ src/Fitness/AED_ML.py
@@ -8,8 +8,8 @@ import os
 def Calculate_AED_ML(data_d,data_r,input,Path,flag_output) :
     species = [species for species, values in input.items() if values["AED"] == 1]
     
-    data_d[species]=data_d[species].apply(np.log)  
-    data_r[species]=data_r[species].apply(np.log) 
+    data_d[species+["T"]]=data_d[species+["T"]].apply(np.log)  
+    data_r[species+["T"]]=data_r[species+["T"]].apply(np.log) 
     
     scl = StandardScaler()
     scl.fit(data_d[species+["T"]])
